690fe9d593fe32387233ac9517f8308591365996
Diego Luerino <dluerino@gmail.com>
2024-11-05 16:37:55 -0300

    new

--- src/src/Clientes/Cliente.java
+++ src/src/Clientes/Cliente.java
@@ -2,7 +2,53 @@ package Clientes;
 
 import Personas.Persona;
 
+import java.util.Objects;
+
 public class Cliente extends Persona {
     private String nacionalidad;
-    private Domicilio domicilio
+    private Domicilio domicilio;
+    private String dni;
+
+    public Cliente(String nombre, String apellido, String gmail, String nacionalidad, Domicilio domicilio, String dni) {
+        super(nombre, apellido, gmail);
+        this.nacionalidad = nacionalidad;
+        this.domicilio = domicilio;
+        this.dni = dni;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public Domicilio getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(Domicilio domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Cliente cliente)) return false;
+        return Objects.equals(getDni(), cliente.getDni());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getDni());
+    }
 }

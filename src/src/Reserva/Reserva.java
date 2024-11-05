46c33b03878466256db5b6a8cade1594fcd9b86f
Marquito-10 <163759791+Marquito-10@users.noreply.github.com>
2024-11-05 16:40:02 -0300

    n

--- src/src/Reserva/Reserva.java
+++ src/src/Reserva/Reserva.java
@@ -7,6 +7,49 @@ public class Reserva {
     private String fechaCheck-out;
     private Habitacion habitacion;
 
+    public Reserva(Cliente cliente, String fechaCheck, String fechaCheck1, Habitacion habitacion) {
+        this.cliente = cliente;
+        this.fechaCheck = fechaCheck;
+        this.fechaCheck = fechaCheck1;
+        this.habitacion = habitacion;
+    }
+
+    public Reserva() {}
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getFechaCheck() {
+        return fechaCheck;
+    }
+
+    public void setFechaCheck(String fechaCheck) {
+        this.fechaCheck = fechaCheck;
+    }
+
+    public Habitacion getHabitacion() {
+        return habitacion;
+    }
+
+    public void setHabitacion(Habitacion habitacion) {
+        this.habitacion = habitacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Reserva{" +
+                "cliente=" + cliente +
+                ", fechaCheck='" + fechaCheck + '\'' +
+                ", fechaCheck='" + fechaCheck + '\'' +
+                ", habitacion=" + habitacion +
+                '}';
+    }
+
 
 
 }

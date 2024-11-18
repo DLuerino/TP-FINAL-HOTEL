3974057dd72b90e091fac8fbc74752a75460b8b7
Facundo Galeano <GALEANOFACUNDO752@GMAIL.COM>
2024-11-18 13:28:45 -0300

    ACTUALIZACION

--- src/src/Contenedores/GestionReserva.java
+++ src/src/Contenedores/GestionReserva.java
@@ -11,7 +11,7 @@ import java.util.HashSet;
 public class GestionReserva {
     private HashMap<String, HashSet<Reserva>> listaReservas;
     private ArrayList<Habitacion> listaHabitaciones;
-    // ------------------------------------------------------------------------------------------------------------
+    /// -----------------------------------------*-----------------------------------------
     public GestionReserva(HashMap<String, HashSet<Reserva>> listaReservas) {
         this.listaReservas = new HashMap<>();
         this.listaHabitaciones=new ArrayList<>();
@@ -21,63 +21,15 @@ public class GestionReserva {
         this.listaReservas = new HashMap<>();
         this.listaHabitaciones=new ArrayList<>();
     }
-    // ------------------------------------------------------------------------------------------------------------
-
-    public HashMap<String, HashSet<Reserva>> getListaReservas() {
-        return listaReservas;
-    }
+    /// -----------------------------------------*-----------------------------------------
 
 
-    public ArrayList<Habitacion> getListaHabitaciones() {
-        return listaHabitaciones;
-    }
-
-
-    public void addHabitacion(Habitacion habitacion){
-        listaHabitaciones.add(habitacion);
-    }
-
-    public void addReserva(Reserva reserva) throws NoDisponibleException {
-        /// verificar disponibilidad
-        ArrayList<Habitacion> habDisponibles=new ArrayList<>();
-
-        for(Habitacion habitacion : listaHabitaciones){
-
-
-        }
-
-        /// DUDA: COMO MANEJAR LA DISPONIBILIDAD POR FECHAS, TENIENDO EN CUENTA QUE DESDE EL CHECK-IN HASTA EL CHECK-OUT ESTA OCUPADA, PERO DESPUES DE ESE PLAZO YA DEBERIA ESTAR DISPONIBLE
-        /// PREGUNTR ESO;
-
-//        for(Reserva res : listaReservas){
-//            if (reserva.getCheckIn().compareTo(reserva.getCheckOut()) < 0 && reserva.getCheckOut().compareTo(reserva.getCheckIn()) > 0){
-//                habDisponibles.add(new Habitacion(reserva.getNumeroHabitacionReservada(), EstadoHabitacion.DISPONIBLE));
-//            }
-//        }
-
-        if(habDisponibles.isEmpty()){
-            throw new NoDisponibleException("No hay habitaciones disponibles para estas fechas!");
-        }
-
-        Habitacion seleccionada= habDisponibles.get(0);
-        reserva.setNumeroHabitacionReservada(seleccionada.getNumeroHabitacion());
-
-        ///verifica si el cliente ya tiene alguna reserva
-        if(!listaReservas.containsKey(reserva.getDniCliente())){
-            ///por que no existe crea uno
-            listaReservas.put(reserva.getDniCliente(), new HashSet<>());
-        }
-
-        ///agrega la reserva al conjunto del cliente
-        listaReservas.get(reserva.getDniCliente()).add(reserva);
-
-        seleccionada.setEstado(EstadoHabitacion.OCUPADA);
-
-    }
 
     public HashSet<Reserva> getReservasXcliente(String dniCliente){
         return listaReservas.getOrDefault(dniCliente, new HashSet<>());
     }
+
+
 
     public String removeReserva(String dniCliente, Reserva reserva){
         String msj="Error al eliminar la reserva! Intente de nuevo por favor.";
@@ -94,6 +46,10 @@ public class GestionReserva {
 
         return msj;
     }
+
+
+
+
 
     public String mostrarTodasLasReservas(){
         StringBuilder sb=new StringBuilder();
--- src/src/Reserva/Habitacion.java
+++ src/src/Reserva/Habitacion.java
@@ -8,16 +8,13 @@ public class Habitacion {
 
     private int numeroHabitacion;
     private EstadoHabitacion estado;
-    private ArrayList<Reserva> listaReservas;
     /// -----------------------------------------------------------------------------------------------------------------
 
     public Habitacion(int numeroHabitacion, EstadoHabitacion estado) {
         this.numeroHabitacion = numeroHabitacion;
         this.estado = estado;
-        this.listaReservas=new ArrayList<>();
     }
 
-    public Habitacion() {this.listaReservas=new ArrayList<>();}
     /// -----------------------------------------------------------------------------------------------------------------
 
     public int getNumeroHabitacion() {
@@ -36,20 +33,6 @@ public class Habitacion {
         this.estado = estado;
     }
 
-    ///Verificar si la habitación está disponible para un rango de fechas
-    public boolean estaDisponible(String checkIn, String checkOut) {
-        if (estado != EstadoHabitacion.DISPONIBLE) {
-            return false;  /// Si la habitación no está disponible, retornamos false
-        }
-
-        for (Reserva reserva : listaReservas) {
-            if (reserva.getCheckIn().compareTo(checkOut) < 0 && reserva.getCheckOut().compareTo(checkIn) > 0) {
-                return false;  /// La habitación ya está ocupada en el rango de fechas
-            }
-        }
-
-        return true;  /// La habitación está disponible
-    }
 
     /// -----------------------------------------------------------------------------------------------------------------
     @Override
